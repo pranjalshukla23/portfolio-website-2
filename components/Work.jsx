2f782f03fdbd758d737fa9b01a6152d7c702f8c1
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2023-01-10 02:31:45 +0530

    uninstalled libraries

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
 
 export const Work = ({ projects }) => {
   const getStack = (msg) => {
