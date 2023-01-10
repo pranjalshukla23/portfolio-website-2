f907f45efb34d6235226bb7dddb25bffe1234554
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2023-01-11 04:03:30 +0530

    font changes in intro 1

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -4,10 +4,10 @@ export const Intro = () => {
       id="intro"
       className="bg-hero-pattern bg-center md:bg-right-top bg-cover bg-no-repeat h-screen flex flex-col justify-center items-center text-white text-center"
     >
-      <h1 className="font-Anton font-medium text-4xl md:text-9xl tracking-widest uppercase py-6">
+      <h1 className="font-Anton font-medium text-4xl md:text-8xl tracking-widest uppercase py-6">
         Pranjal Shukla
       </h1>
-      <h3 className="font-Montserrat font-bold text-md md:text-2xl tracking-widest uppercase">
+      <h3 className="font-Montserrat font-bold text-md md:text-xl tracking-widest uppercase">
         Software engineer, Front end & App Developer.
       </h3>
     </div>
