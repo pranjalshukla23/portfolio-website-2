98080845febb22a2a65bdea46831c400d52e9d63
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2023-01-11 18:15:55 +0530

    added loader

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
-export const Intro = () => {
-  const [loading, setLoading] = useState(true);
+import React from "react";
 
+export const Intro = ({ loading, setLoading }) => {
+  const onLoad = () => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
   return (
     <div
       id="intro"
@@ -14,21 +18,16 @@ export const Intro = () => {
         className="video h-full"
         loop
         src="/images/bg-video-2.mp4"
-        onPlay={() => {
-          console.log("loaded");
-          setLoading(false);
-        }}
+        onPlay={onLoad}
       />
-      {!loading && (
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-inherit flex flex-col justify-center items-center">
-          <h1 className="font-Anton font-medium text-4xl md:text-8xl tracking-widest uppercase mt-4 py-6">
-            Pranjal Shukla
-          </h1>
-          <h3 className="font-Montserrat font-bold text-xs md:text-xl tracking-widest uppercase">
-            Software engineer, Front end & App Developer.
-          </h3>
-        </div>
-      )}
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-inherit flex flex-col justify-center items-center">
+        <h1 className="font-Anton font-medium text-4xl md:text-8xl tracking-widest uppercase mt-4 py-6">
+          Pranjal Shukla
+        </h1>
+        <h3 className="font-Montserrat font-bold text-xs md:text-xl tracking-widest uppercase">
+          Software engineer, Front end & App Developer.
+        </h3>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -5,24 +5,47 @@ import { Work } from "../components/Work";
 import projects from "../data/projects.json";
 import { Contact } from "../components/Contact";
 import { Experience } from "../components/Experience";
+import { useState } from "react";
+import {
+  CircleLoader,
+  ClipLoader,
+  RingLoader,
+  RotateLoader,
+} from "react-spinners";
 
 export default function Home({ projects }) {
+  const [loading, setLoading] = useState(true);
+
   return (
     <div>
       <Head>
         <title>Pranjal Shukla - Software Engineer</title>
       </Head>
-      <Intro />
-      <Expertise />
-      <Work projects={projects} />
-      <Experience />
-      <Contact />
+
+      <Intro loading={loading} setLoading={setLoading} />
+      {loading ? (
+        <div className="text-center h-screen">
+          <ClipLoader
+            color={"#fff"}
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <>
+          <Expertise />
+          <Work projects={projects} />
+          <Experience />
+          <Contact />
+        </>
+      )}
     </div>
   );
 }
 
 export async function getStaticProps(context) {
-  //console.log("projects", projects);
   return {
     props: {
       projects,
