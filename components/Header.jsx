021124071435943ef8777fb47a4dd5baf978ebbd
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2023-01-11 14:25:25 +0530

    resolved link issues with a tag and href

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,4 @@
 export const Header = () => {
-  const handleClickScroll = (id) => {
-    const element = document.getElementById(id);
-    if (element && id !== "intro") {
-      // 👇 Will scroll smoothly to the top of the next section
-      element.scrollIntoView({
-        block: "start",
-      });
-    } else if (element && id === "intro") {
-      console.log("intro");
-      // 👇 Will scroll smoothly to the top of the next section
-      element.scrollIntoView({
-        block: "center",
-      });
-    }
-  };
   return (
     <header className="flex flex-col md:flex-row justify-start items-center gap-x-48 bg-black p-4 sticky top-0 z-40">
       <h1 className="font-RobotoCondensed text-4xl text-blue-300">
@@ -21,23 +6,41 @@ export const Header = () => {
       </h1>
       <ul className="flex flex-col md:flex-row justify-between items-center gap-2 text-white text-lg font-Montserrat font-bold w-1/2">
         <li>
-          <button onClick={() => handleClickScroll("intro")}>Home</button>
+          <a href="#" className="hover:text-green-400 focus:text-green-400">
+            Home
+          </a>
         </li>
         <li>
-          <button onClick={() => handleClickScroll("expertise")}>
-            Expertise
-          </button>
+          <a
+            href="#expertise"
+            className="hover:text-green-400 focus:text-green-400"
+          >
+            {" "}
+            Expertise{" "}
+          </a>
         </li>
         <li>
-          <button onClick={() => handleClickScroll("work")}>Work</button>
+          <a href="#work" className="hover:text-green-400 focus:text-green-400">
+            {" "}
+            Work{" "}
+          </a>
         </li>
         <li>
-          <button onClick={() => handleClickScroll("experience")}>
-            Experience
-          </button>
+          <a
+            href="#experience"
+            className="hover:text-green-400 focus:text-green-400"
+          >
+            {" "}
+            Experience{" "}
+          </a>
         </li>
         <li>
-          <button onClick={() => handleClickScroll("contact")}>Contact</button>
+          <a
+            href="#contact"
+            className="hover:text-green-400 focus:text-green-400"
+          >
+            Contact{" "}
+          </a>
         </li>
       </ul>
     </header>
