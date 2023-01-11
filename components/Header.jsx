c5a8cbf1a131b4e2c08722f9382d8f466efc41fe
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2023-01-11 14:51:00 +0530

    added active link logic

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export const Header = () => {
+  const [activeLink, setActiveLink] = useState("home");
+
   return (
     <header className="flex flex-col md:flex-row justify-start items-center gap-x-48 bg-black p-4 sticky top-0 z-40">
       <h1 className="font-RobotoCondensed text-4xl text-blue-300">
@@ -6,21 +10,36 @@ export const Header = () => {
       </h1>
       <ul className="flex flex-col md:flex-row justify-between items-center gap-2 text-white text-lg font-Montserrat font-bold w-1/2">
         <li>
-          <a href="#" className="hover:text-green-400 focus:text-green-400">
+          <a
+            href="#"
+            className={`hover:text-green-400 ${
+              activeLink === "home" ? "text-green-400" : ""
+            }`}
+            onClick={() => setActiveLink("home")}
+          >
             Home
           </a>
         </li>
         <li>
           <a
             href="#expertise"
-            className="hover:text-green-400 focus:text-green-400"
+            className={`hover:text-green-400 ${
+              activeLink === "expertise" ? "text-green-400" : ""
+            }`}
+            onClick={() => setActiveLink("expertise")}
           >
             {" "}
             Expertise{" "}
           </a>
         </li>
         <li>
-          <a href="#work" className="hover:text-green-400 focus:text-green-400">
+          <a
+            href="#work"
+            className={`hover:text-green-400 ${
+              activeLink === "work" ? "text-green-400" : ""
+            }`}
+            onClick={() => setActiveLink("work")}
+          >
             {" "}
             Work{" "}
           </a>
@@ -28,7 +47,10 @@ export const Header = () => {
         <li>
           <a
             href="#experience"
-            className="hover:text-green-400 focus:text-green-400"
+            className={`hover:text-green-400 ${
+              activeLink === "experience" ? "text-green-400" : ""
+            }`}
+            onClick={() => setActiveLink("experience")}
           >
             {" "}
             Experience{" "}
@@ -37,7 +59,10 @@ export const Header = () => {
         <li>
           <a
             href="#contact"
-            className="hover:text-green-400 focus:text-green-400"
+            className={`hover:text-green-400 ${
+              activeLink === "contact" ? "text-green-400" : ""
+            }`}
+            onClick={() => setActiveLink("contact")}
           >
             Contact{" "}
           </a>
