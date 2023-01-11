379037b5aacf5fc5509193396ded0d3d2319d63d
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2023-01-12 03:39:41 +0530

    changed font size of other projects header

--- components/Work.jsx
+++ components/Work.jsx
@@ -8,7 +8,7 @@ export const Work = ({ projects }) => {
 
   return (
     <div className="flex flex-col flex-wrap justify-center items-center text-white mt-24 text-center">
-      <h1 className="font-Anton font-extrabold text-3xl  tracking-widest uppercase">
+      <h1 className="font-Anton text-2xl  tracking-widest uppercase">
         Other Noteworthy Projects
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 w-full lg:w-2/3 p-12 gap-16">
