1599e8fb105f3e391b44e56469276a3d5a0afa3e
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2023-01-13 14:43:00 +0530

    changes in linear gradient

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -56,8 +56,10 @@ export const Intro = () => {
       >
         {getVideo()}
         <div
-          className={`flex absolute top-0 left-0 right-0 bottom-0  flex-col justify-center items-center bg-gradient-to-br from-gray-600
-           via-black to-black md:bg-none ${!loading ? "md:flex" : "md:hidden"}`}
+          className={`flex absolute top-0 left-0 right-0 bottom-0  flex-col justify-center items-center bg-gradient-to-br from-black
+           via-gray-900 to-black md:bg-none ${
+             !loading ? "md:flex" : "md:hidden"
+           }`}
         >
           <h1 className="font-Anton font-medium text-4xl md:text-8xl tracking-widest uppercase  py-6">
             Pranjal Shukla
