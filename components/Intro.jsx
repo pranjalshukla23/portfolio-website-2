51a35009f1e5326a4725491551c636f7edf8fcae
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2023-01-16 01:49:42 +0530

    single video for all screens

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -31,18 +31,9 @@ export const Intro = () => {
           autoPlay
           playsInline
           muted
-          className={`video h-full hidden md:block`}
+          className={`video h-full brightness-50`}
           loop
-          src="/images/bg-video-2.mp4"
-          onPlay={onLoad}
-        />
-        <video
-          autoPlay
-          playsInline
-          muted
-          className={`video h-full block md:hidden`}
-          loop
-          src="/images/mobile-video.mp4"
+          src="/images/bg-video-sample-1.mp4"
           onPlay={onLoad}
         />
       </>
@@ -70,12 +61,12 @@ export const Intro = () => {
       >
         {getVideo()}
         <div
-          className={`hidden absolute top-0 left-0 right-0 bottom-0 flex-col justify-center items-center transition-[blur] duration-1000 delay-1000 ease-in bg-gradient-to-br from-black via-gray-900 to-black md:bg-none ${
+          className={`flex absolute top-0 left-0 right-0 bottom-0 flex-col justify-center items-center transition-[blur] duration-1000 delay-1000 ease-in  md:bg-none ${
             !loading ? "md:flex" : "md:hidden"
           }   ${blur ? "blur-lg" : "blur-none"}`}
         >
           <h1
-            className={`font-Anton font-medium text-6xl text-white md:text-8xl tracking-widest uppercase  py-6 `}
+            className={`font-Anton font-medium text-4xl text-white md:text-8xl tracking-widest uppercase  py-6 `}
           >
             Pranjal Shukla
           </h1>
