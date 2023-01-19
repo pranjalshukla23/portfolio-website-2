66ce0bf438ab51e8dc9a7c3996df42c632d35f9b
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2023-01-19 23:40:36 +0530

    added images for featured section in mobile

--- components/FeaturedItem.jsx
+++ components/FeaturedItem.jsx
@@ -9,8 +9,8 @@ export const FeaturedItem = ({ project }) => {
 
   return (
     <div
-      className={`flex ${
-        project.id % 2 === 0 ? "flex-row" : "flex-row-reverse"
+      className={`flex flex-col ${
+        project.id % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"
       } justify-center items-center w-full h-full md:w-3/4  my-4 gap-6 `}
       key={project.id}
     >
@@ -18,20 +18,20 @@ export const FeaturedItem = ({ project }) => {
       <Link
         href={project.github ? project.github : project.link}
         key={project.id}
-        className="w-full hidden md:block"
+        className="w-4/5 md:w-full md:block"
       >
         <Image
           src={project.imageUrl}
           alt="image"
-          width={800}
+          width={600}
           height={400}
           className="border-4 border-gray-900 rounded-md hover:scale-105"
         />
       </Link>
       {/*  information section*/}
       <div
-        className={`flex flex-col justify-center ${
-          project.id % 2 === 0 ? "items-end" : "items-start"
+        className={`flex flex-col justify-center items-center ${
+          project.id % 2 === 0 ? "md:items-end" : "md:items-start"
         } w-full text-white`}
       >
         <Link
@@ -45,7 +45,7 @@ export const FeaturedItem = ({ project }) => {
             <i className="fa-solid fa-arrow-up-right-from-square"></i>
           )}
         </Link>
-        <div className="bg-slate-900 rounded-md border-2 border-gray-50 shadow-lg shadow-slate-600 p-4 w-full">
+        <div className="bg-slate-900 rounded-md border-2 border-gray-50 shadow-lg shadow-slate-600 p-4 w-full hidden md:block">
           <p className="text-slate-400 font-bold">{project.description}</p>
           <p className="hidden md:block overflow-hidden break-words text-gray-400 mt-8 text-center">
             {getStack(project.tools)}
